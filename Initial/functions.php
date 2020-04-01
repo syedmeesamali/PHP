72eac29b6c87a35cc1f7c4d19924af184c0b54dd
syedmeesamali <syedmeesamali@yahoo.com>
2020-04-01 06:24:16 +0400

    arrays, functions and get

---
+++ Initial/functions.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php 
+ function name() {
+     echo 'meesam ali';
+ }
+ name();
+ 
+ ?> 
+ </body>
+</html>
---
+++ Initial/get.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php 
+ $name = $_GET['name'];
+ $age = $_GET['age'];
+ echo 'My name is '.$name.' and my age is '.$age;
+ ?> 
+ </body>
+</html>
