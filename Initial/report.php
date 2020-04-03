7e4cc5614ce6d2dac487140398c173776c56ab45
syedmeesamali <syedmeesamali@yahoo.com>
2020-04-03 23:46:20 +0400

    Email functionality added to the report php form

--- Initial/report.php
+++ Initial/report.php
@@ -6,12 +6,21 @@
  
 <?php
  $whenithappened = $_POST['whenithappened'];
+ $other = $_POST['other'];
  $fang_spotted = $_POST['fangspotted'];
  $email = $_POST['email'];
- 
+
+ $to = 'meeghou8689@gmail.com';
+ $subject = 'Aliens abducted me!!';
+ $msg = "Hello my name is ali and my dog name is fang. \n"."The incident happened $whenithappened\n"."Was
+ my fang spotted? The answer is $fang_spotted\n"."Some other information is as below:\n"." Other info: 
+    $other";
+
+ mail($to, $subject, $msg, 'From: '.$email);
  echo 'Thanks for submitting form <br />';
  echo 'Was fang spotted?  <br />'.$fang_spotted;
- echo 'Your email address is:  <br />'.$email;
+ echo '<br />Your email address is:  <br />'.$email;
+ echo '<br />An email report also sent to your provided email id:  <br />'.$email;
 ?>
 
 </body>
