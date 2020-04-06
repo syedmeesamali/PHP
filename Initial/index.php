c0d2788848d8dca40737ebf70dfa7f46d6d93996
syedmeesamali <syedmeesamali@yahoo.com>
2020-04-06 23:50:00 +0400

    Password input using the post method

--- Initial/index.php
+++ Initial/index.php
@@ -3,16 +3,22 @@
   <title>main index file</title>
  </head>
  <body>
- <h2><?php echo '<p>Hello to PHP</p>'; ?> </h2><br>
- <?php
-$password = 'alishah';
-if ($password == 'alishah')
-{
-    echo "<h2 style='color:blue';>Correct Password!</h2>";
-} else {
-    echo "<h2 style='color:red';>Wrong Password!</h2>";
-}
+<form action="index.php" method="POST">
+Please enter your password: <br>
+<input type="password" name="password">
+<input type="submit" value="submit">
 
+</form>
+<?php
+$password = 'password';
+if (isset($_POST['password'])&&!empty($_POST['password'])) {
+    if ($_POST['password'] == $password)
+    {
+        echo 'Correct password indeed!';
+    } else {
+        echo 'Incorrect password';
+    }
+}
 ?>
  </body>
 </html>
