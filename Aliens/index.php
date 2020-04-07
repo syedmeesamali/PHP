c84d2e2b5c206a4cba4ab74844e06abbb349f973
syedmeesamali <syedmeesamali@yahoo.com>
2020-04-07 17:01:06 +0400

    Database upated successfully with mysql queries from PHP
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- Aliens/index.php
+++ Aliens/index.php
@@ -6,6 +6,17 @@
 <?php
 $dbc = mysqli_connect('localhost', 'root', '', 'main_db') 
 or die('Error connecting to MySql database.');
+
+$query = "INSERT INTO alienabduction (first_name, last_name, when_it, how_long, description,".
+"what_did, spotted, other, email) VALUES ('meesam', 'ali', 'friday', '10 hours', 'aliens in green robes', ".
+"'we talked and played with dog', 'yes', 'i may have seen him. contact me', 'meesamali@mymail.com')";
+
+$result = mysqli_query($dbc, $query)
+or die('Error querying the database.');
+
+mysqli_close($dbc);
+echo 'Successfully database updated!';
+
 ?>
  </body>
 </html>
