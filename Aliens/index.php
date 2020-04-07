a95889bbf1426b66b8c43a11747b822e8cf2e7ce
syedmeesamali <syedmeesamali@yahoo.com>
2020-04-07 16:48:14 +0400

    DB connection successful with mysql - PHP work continues from the book
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

---
+++ Aliens/index.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>main index file</title>
+ </head>
+ <body>
+<?php
+$dbc = mysqli_connect('localhost', 'root', '', 'main_db') 
+or die('Error connecting to MySql database.');
+?>
+ </body>
+</html>
