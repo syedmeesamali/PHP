7ad5fb3adbcec0205ac88c38bb8e26c3084c983c
syedmeesamali <syedmeesamali@yahoo.com>
2020-04-14 22:05:18 +0400

    Functions with arrays

--- Initial/arrays.php
+++ Initial/arrays.php
@@ -4,12 +4,26 @@
  </head>
  <body>
  <?php 
- $names = array(
- 'ali'=>array('Age'=>24, 'Hair'=>'Blonde'), 
- 'ahmed'=>array('Age'=>29, 'Hair'=>'Black'), 
- 'shah'=>array('Age'=>34, 'Hair'=>'Brown'));
- print_r($names);
+ $GLOBALS['names'] = array(
+ 'ali'=>array('Age'=>34, 'Hair'=>'Blonde'), 
+ 'ahmed'=>array('Age'=>24, 'Hair'=>'Black'), 
+ 'shah'=>array('Age'=>36, 'Hair'=>'Brown'));
+ 
+ echo $GLOBALS['names']['ali']['Age'];
+ 
  echo '<pre>', print_r($names, true), '</pre>';
+
+ function array_data($names, $data)
+ {
+     if (array_key_exists($names, $GLOBALS['names']) == true)
+     {
+         return $GLOBALS['names'][$names][$data];
+     } else {
+         return false;
+     }
+ }
+
+ echo array_data('ali', 'Age');
  ?> 
  </body>
 </html>
