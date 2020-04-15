7235d0da0f83d14380551230f2b9c21b4757e5af
syedmeesamali <syedmeesamali@yahoo.com>
2020-04-15 23:43:58 +0400

    set and unset a cookie

--- Initial/cookies.php
+++ Initial/cookies.php
@@ -8,8 +8,19 @@
 //$exp = time() + 86400;
 //setcookie("name", "meesam", $exp);
 
-echo $_COOKIE["name"];
-var_dump($_COOKIE);
+//echo $_COOKIE["name"];
+
+
+if (isset($_COOKIE['name']))
+{
+    echo "COokie is set:";
+} else {
+    echo "Cookie not set";
+}
+
+$exp_unset = time() - 86400;
+//Unset a cookie
+setcookie("name", "", $exp_unset);
 
 ?> 
 </body>
