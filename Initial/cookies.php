2629359cabfa63525e6f016bf76e377f5f61c766
syedmeesamali <syedmeesamali@yahoo.com>
2020-04-15 23:07:45 +0400

    Cookies in php

---
+++ Initial/cookies.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>PHP Test</title>
+</head>
+<body>
+<?php 
+
+//$exp = time() + 86400;
+//setcookie("name", "meesam", $exp);
+
+echo $_COOKIE["name"];
+var_dump($_COOKIE);
+
+?> 
+</body>
+</html>
