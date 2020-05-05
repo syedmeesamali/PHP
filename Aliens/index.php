11f90a25571d2bad8796b7a136377329add3df8d
syedmeesamali <syedmeesamali@yahoo.com>
2020-05-05 16:06:14 +0400

    DB connection and display of results from mysql
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- Aliens/index.php
+++ Aliens/index.php
@@ -7,15 +7,17 @@
 $dbc = mysqli_connect('localhost', 'root', '', 'main_db') 
 or die('Error connecting to MySql database.');
 
-$query = "INSERT INTO alienabduction (first_name, last_name, when_it, how_long, description,".
-"what_did, spotted, other, email) VALUES ('meesam', 'ali', 'friday', '10 hours', 'aliens in green robes', ".
-"'we talked and played with dog', 'yes', 'i may have seen him. contact me', 'meesamali@mymail.com')";
-
+$query = "SELECT * FROM alienabduction";
 $result = mysqli_query($dbc, $query)
-or die('Error querying the database.');
+or die('Error connecting to MySql database.');
+
+while ($row = mysqli_fetch_array($result)) {
+    $first_name = $row['first_name'];
+    $last_name = $row['last_name'];
+    echo "Name: ".$first_name." & last name: ".$last_name." sent the message. <br>";
+}
 
 mysqli_close($dbc);
-echo 'Successfully database updated!';
 
 ?>
  </body>
